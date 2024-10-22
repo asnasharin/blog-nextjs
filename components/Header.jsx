a3997b9e665af3da67e7842b5bf683f0e59b34e0
asna sharin <asnasherin1233@gmail.com>
2024-10-22 18:52:21 +0530

    add signout

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,15 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
-import { auth } from '@/lib/auth';
+import { signOut, useSession } from 'next-auth/react';
 
-const Header = async () => {
-    
-    const session = await auth();
+const Header = () => {
+    const { data: session } = useSession();
+
+    const handleLogout = () => {
+        signOut({ callbackUrl: '/login' });
+    };
 
     return (
         <div className='py-5 px-5 md:px-12 lg:px-28'>
@@ -22,9 +27,7 @@ const Header = async () => {
                     </li>
                     {session?.user?.email === 'admin@gmail.com' && (
                         <div>
-                            <Link href='/admin'>
-                            Admin
-                            </Link>
+                            <Link href='/admin'>Admin</Link>
                         </div>
                     )}
                 </ul>
@@ -32,7 +35,13 @@ const Header = async () => {
                     <Link href='/register'>Get started</Link>
                 </button>
                 <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>
-                    <Link href='/login'>login</Link>
+                    <Link href='/login'>Login</Link>
+                </button>
+                <button 
+                    className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]' 
+                    onClick={handleLogout}
+                >
+                    Logout
                 </button>
             </div>
         </div>
